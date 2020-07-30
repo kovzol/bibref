1b7a2b960b4896b738fa54522fb585867892916c
Zoltán Kovács <zoltan@geogebra.org>
2020-07-30 12:25:46 +0200

    Add research data and clarify some former results

--- docs/common/psalms.sql
+++ docs/common/psalms.sql
@@ -24,22 +24,24 @@ create table quotations (
  nt_length int,
  found_method text);
 
-insert into authors (name, info) values ('Paul', 'Paul the Apostle');
 insert into authors (name, info) values ('Matthew', 'Matthew the Apostle');
-insert into authors (name, info) values ('Luke', 'Luke the Evangelist');
-insert into authors (name, info) values ('Peter', 'Peter the Apostle');
-insert into authors (name, info) values ('John', 'John the Apostle');
 insert into authors (name, info) values ('Mark', 'Mark the Evangelist');
+insert into authors (name, info) values ('Luke', 'Luke the Evangelist');
+insert into authors (name, info) values ('John', 'John the Apostle');
+insert into authors (name, info) values ('Paul', 'Paul the Apostle');
+insert into authors (name, info) values ('Peter', 'Peter the Apostle');
 insert into authors (name, info) values ('Unknown', 'Author of Hebrews');
 
 insert into found_methods (name, info) values ('traditional', 'known by tradition');
 insert into found_methods (name, info) values ('getrefs', 'found by the getrefs algorithm');
 insert into found_methods (name, info) values ('manual', 'confirmed manually');
 
+insert into books (name, author, info) values ('Matthew', 'Matthew', 'Gospel of Matthew');
+insert into books (name, author, info) values ('Luke', 'Luke', 'Gospel of Luke');
+insert into books (name, author, info) values ('Acts', 'Luke', 'Acts of the Apostles');
 insert into books (name, author, info) values ('Romans', 'Paul', 'Epistle to the Romans');
 insert into books (name, author, info) values ('Ephesians', 'Paul', 'Epistle to the Ephesians');
 insert into books (name, author, info) values ('Hebrews', 'Unknown', 'Epistle to the Hebrews');
-insert into books (name, author, info) values ('Acts', 'Luke', 'Acts of the Apostles');
 
 insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (1, 1, 2, 'Psalms 2:1-2', 'Acts', 'Acts 4:25-26', null, null, null, null, 'traditional');
@@ -73,3 +75,24 @@ insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot
  (3, 1, 4, 'LXX Psalms 4:4 4:4-63', 'Ephesians', 'SBLGNT Ephesians 4:26 4:26-33', 2163, 24, null, 24, 'getrefs');
 insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (3, 1, 4, 'LXX Psalms 4:4 4:4-63', 'Ephesians', 'SBLGNT Ephesians 4:26 4:26-33', 2163, 24, null, 24, 'manual');
+
+insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (4, 1, 6, 'LXX Psalms 6:8 6:8-64', 'Luke', 'SBLGNT Luke 13:27+32 13:27-14', 3985, 20, null, 20, 'getrefs');
+insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (4, 1, 6, 'LXX Psalms 6:8 6:8-64', 'Luke', 'SBLGNT Luke 13:27+32 13:27-14', 3985, 20, null, 20, 'manual');
+
+insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (5, 1, 8, 'Psalms 8:2', 'Matthew', 'Matthew 21:16', null, null, null, null, 'traditional');
+insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (5, 1, 8, 'LXX Psalms 8:2 8:2-47', 'Matthew', 'SBLGNT Matthew 21:16+76 21:16', 5606, 43, null, 43, 'getrefs');
+insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (5, 1, 8, 'LXX Psalms 8:2 8:2-47', 'Matthew', 'SBLGNT Matthew 21:16+76 21:16', 5606, 43, null, 43, 'manual');
+
+insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (6, 1, 8, 'Psalms 8:4-6', 'Hebrews', 'Hebrews 2:6-8', null, null, null, null, 'traditional');
+insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (6, 1, 8, 'LXX Psalms 8:4 8:5', 'Hebrews', 'SBLGNT Hebrews 2:6+25 2:7', 5766, 120, null, 120, 'getrefs');
+insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (6, 1, 8, 'LXX Psalms 8:6+39 8:6', 'Hebrews', 'SBLGNT Hebrews 2:8 2:8-84', 5925, 33, null, 33, 'getrefs');
+insert into quotations (ot_id, nt_id, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (6, 1, 8, 'LXX Psalms 8:4 8:6', 'Hebrews', 'SBLGNT Hebrews 2:6+25 2:8-84', 5766, 192, null, 120+33, 'manual');
