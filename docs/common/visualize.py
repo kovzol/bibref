cb76d9e59ba3995657741ff040212e6e98ffa7d7
Zoltán Kovács <zoltan@geogebra.org>
2020-08-10 19:01:39 +0200

    Add more code for visualization

--- docs/common/visualize.py
+++ docs/common/visualize.py
@@ -25,7 +25,24 @@ def psalms_report_latex(conn):
     """
     cur = conn.cursor()
 
+    print ("\\documentclass{article}")
+    print ("\\usepackage{mathtools}")
+    print ("\\usepackage[dvipsnames]{xcolor}")
+    print ("\\newcommand\\Luke{{\color{Lavender}\\bullet}\mathllap{\color{Lavender}\circ}}")
+    print ("\\newcommand\\Paul{{\color{YellowGreen}\\bullet}\mathllap{\color{YellowGreen}\circ}}")
+    print ("\\newcommand\\Unknown{{\color{Orchid}\\bullet}\mathllap{\color{Orchid}\circ}}")
+    print ("\\newcommand\\Matthew{{\color{Red}\\bullet}\mathllap{\color{Red}\circ}}")
+    print ("\\newcommand\\John{{\color{GreenYellow}\\bullet}\mathllap{\color{GreenYellow}\circ}}")
+    print ("\\begin{document}")
+    print ("\\centering")
+    print ("\\begin{table}")
+    print ("\\begin{tabular}{rcccccccccc}")
+    for i in range(0,10):
+        print ("&{\\bf", i+1, "}", end = '', sep = '')
+    print ("\\\\")
     for psalm in range(1,151):
+        if psalm % 10 == 1:
+            print ("{\\bf ", round(psalm/10), "}&", end = '', sep = '')
         cur.execute("SELECT q.ot_id, q.nt_id, q.nt_book, a.name" +
             " FROM quotations_classifications qc, quotations q, authors a, books b " +
             " WHERE qc.classification = 'quotation'" +
@@ -38,9 +55,27 @@ def psalms_report_latex(conn):
             " GROUP BY q.ot_id, q.nt_id")
         rows = cur.fetchall()
         if len(rows) > 0:
-            print ("Psalm", psalm)
+            print
+            print ("\n", "% Psalm ", psalm, sep = '')
+            old_ot_id = 0
             for row in rows:
-                print(row)
+                print("%", row)
+                author = row[3]
+                ot_id = row[0]
+                if old_ot_id > 0 and ot_id != old_ot_id:
+                    print(",", sep = '', end = '')
+                # if author == "Luke":
+                #     print ("$\\Luke$", end = '')
+                print ("$\\", author, "$", sep = '', end = '')
+                old_ot_id = ot_id
+        if psalm % 10 == 0:
+            print ("\\\\")
+        else:
+            print ("&", end = '', sep = '')
+    print ("\\end{tabular}")
+    print ("\\end{table}")
+    print ("\\end{document}")
+
 
 def main():
     database = r"psalms.sqlite3"
