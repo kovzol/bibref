63dceec2c264493d35ee77411d44ef9b70d27ee8
Zoltán Kovács <zoltan@geogebra.org>
2020-12-26 13:23:18 +0100

    Add research on Exodus 13:2

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -1471,6 +1471,12 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (2103, 1, 'νομον', 'το γαρ'); -- νομον = the Law (in Romans 13:8), το γαρ = for
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (2103, 1, 'quotation', 'literal', 'Z.K.');
+insert into quotations (ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (2104, 1, 'Exodus', null, 'LXX Exodus 13:2 13:2-47', 'Luke', 'SBLGNT Luke 2:23+29 2:23-17', 36402, 53, 7753, 28, 'manual');
+insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_given, as_it_is_written) values
+ (2104, 1, 'εν νομω κυριου', 'καθως γεγραπται'); -- εν νομω κυριου = in the law of the Lord, καθως γεγραπται = as it has been written
+insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
+ (2104, 1, 'quotation', 'superset, synonym, far-fuzzy', 'Z.K.');
 
 insert into quotations (ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (2201, 1, 'Leviticus', null, 'LXX Leviticus 18:5+69 18:5-21', 'Romans', 'SBLGNT Romans 10:5+46 10:5-1', 57053, 27, 20844, 27, 'getrefs');
