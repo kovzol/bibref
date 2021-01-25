be145656ce97634de7347882be016373f299d05e
Zoltán Kovács <zoltan@geogebra.org>
2021-01-25 08:50:41 +0100

    Add research on Ephesians 6:2-3

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -1670,6 +1670,18 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (2107, 1, null, 'καθως γεγραπται'); -- καθως γεγραπται = as it has been written
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (2107, 1, 'quotation', 'literal, synonym, fuzzy', 'Z.K.');
+insert into quotations (ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (2108, 1, 'Exodus', null, 'LXX Exodus 20:12+27 20:12-63', 'Ephesians', 'SBLGNT Ephesians 6:2+58 6:3-24', 57231, 19, 10174, 19, 'getrefs');
+insert into quotations (ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (2108, 1, 'Exodus', null, 'LXX Exodus 20:12 20:12-35', 'Ephesians', 'SBLGNT Ephesians 6:2 6:3', 57204, 74, 10116, 101, 'manual');
+insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_given, as_it_is_written) values
+ (2108, 1, 'εντολη', null); -- εντολη = commandment
+insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
+ (2108, 1, 'quotation', 'literal, exact, subset', 'Z.K.');
+-- TODO: Add also these occurrences:
+-- Found in Mark 7:10+14 7:10-44 (book position 21570-21597)
+-- Found in Mark 10:19+74 10:19 (book position 32525-32552)
+-- Found in Luke 18:20+61 18:20 (book position 70644-70671)
 
 insert into quotations (ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (2201, 1, 'Leviticus', null, 'LXX Leviticus 18:5+69 18:5-21', 'Romans', 'SBLGNT Romans 10:5+46 10:5-1', 57053, 27, 20844, 27, 'getrefs');
