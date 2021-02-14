69588e767ed7f119dd076fc3d1bd646d31c5cdf2
Zoltán Kovács <zoltan@geogebra.org>
2021-02-14 17:18:07 +0100

    Add research on Exodus 21:17

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -2000,6 +2000,14 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (2112, 1, 'θεος', 'ειπεν'); -- ειπεν = commanded
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (2112, 1, 'quotation', 'literal, superset, fuzzy', 'Z.K.');
+insert into quotations (ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (2112, 2, 'Exodus', null, 'LXX Exodus 21:17 21:17-33', 'Mark', 'SBLGNT Mark 7:10+48 7:10-22', 59825, 16, 21604, 16, 'getrefs');
+insert into quotations (ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (2112, 2, 'Exodus', null, 'LXX Exodus 21:17', 'Mark', 'SBLGNT Mark 7:10+48 7:10', 59825, 49, 21604, 38, 'manual');
+insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_given, as_it_is_written) values
+ (2112, 2, 'Μωυσης', 'γαρ ειπεν'); -- γαρ ειπεν = for said
+insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
+ (2112, 2, 'quotation', 'literal, superset, fuzzy', 'Z.K.');
 
 insert into quotations (ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (2201, 1, 'Leviticus', null, 'LXX Leviticus 18:5+69 18:5-21', 'Romans', 'SBLGNT Romans 10:5+46 10:5-1', 57053, 27, 20844, 27, 'getrefs');
