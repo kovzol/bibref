133b93ee223610aa60e28cc9e53d4306322ddee3
Zoltán Kovács <zoltan@geogebra.org>
2021-02-28 11:35:33 +0100

    Add research on Luke 20:37

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -2180,6 +2180,12 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (2113, 2, 'εν τη βιβλω Μωυσεως', 'λεγων'); -- λεγων = saying
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (2113, 2, 'quotation', 'literal, superset, fuzzy', 'Z.K.');
+insert into quotations (ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
+ (2113, 3, 'Exodus', null, 'LXX Exodus 3:6+33 3:6-70', 'Luke', 'SBLGNT Luke 20:37+64 20:37', 5011, 37, 78887, 37, 'manual');
+insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_given, as_it_is_written) values
+ (2113, 3, 'Μωυσης', 'λεγει'); -- λεγει = calls
+insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
+ (2113, 3, 'quotation', 'literal, grammatical-change, fuzzy', 'Z.K.');
 
 insert into quotations (ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (2201, 1, 'Leviticus', null, 'LXX Leviticus 18:5+69 18:5-21', 'Romans', 'SBLGNT Romans 10:5+46 10:5-1', 57053, 27, 20844, 27, 'getrefs');
