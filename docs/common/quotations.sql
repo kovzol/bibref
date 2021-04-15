f61853cfd291687901a2b8801e73909194f60233
Zoltán Kovács <zoltan@geogebra.org>
2021-04-15 07:14:12 +0200

    Add i/c for Hebrews 2:12

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -448,7 +448,11 @@ insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passag
 insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_given, as_it_is_written) values
  (15, 1, null, 'λεγων'); -- λεγων = saying
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
- (15, 1, 'quotation', 'exact, literal, synonym', 'Z.K.');
+ (15, 1, 'quotation', 'exact, literal, synonym', 'Z.K.'); -- The preceding word διηγησομαι is changed to Απαγγελω (synonym).
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (151, 'Hebrews', 'SBLGNT Hebrews 2:12 2:12-56', 2340, 2344);
+insert into clasps (nt_quotation_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (151, 'Psalms', 'LXX Psalms 22:22+10 22:22', 'Hebrews', 'SBLGNT Hebrews 2:12+13 2:12', 21617, 48, 2353, 48);
 
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (161, 16, 1, 'Psalms', 24, 'Psalms 24:1', 'I_Corinthians', 'I_Corinthians 10:26', null, null, null, null, 'traditional');
