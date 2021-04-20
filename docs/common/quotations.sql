a2094b8c42f5825176f30148fa803188b5b1799c
Zoltán Kovács <zoltan@geogebra.org>
2021-04-20 06:47:18 +0200

    Add i/c data for Romans 8:36

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -620,6 +620,10 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (23, 1, null, 'καθως γεγραπται'); -- καθως γεγραπται = as it has been written
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (23, 1, 'quotation', 'literal', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (231, 'Romans' 'SBLGNT Romans 8:36 8:36-62', 17840, 17853);
+insert into clasps (nt_quotation_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (231, 'Psalms', 'LXX Psalms 44:22', 'Romans', 'SBLGNT Romans 8:36+14 8:36', 49269, 61, 17854, 62);
 
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (241, 24, 1, 'Psalms', 45, 'Psalms 45:6', 'Hebrews', 'Hebrews 1:8', null, null, null, null, 'traditional');
