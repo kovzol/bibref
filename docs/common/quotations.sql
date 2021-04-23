0bc6d80b8c736a670f0fecb25aa978d0184e1464
Zoltán Kovács <zoltan@geogebra.org>
2021-04-23 07:09:39 +0200

    Add i/c data for Ephesians 4:8

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -673,6 +673,10 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (26, 1, null, 'λεγει'); -- λεγει = says
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (26, 1, 'quotation', 'fuzzy, change', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (261, 'Romans', 'SBLGNT Ephesians 4:8 4:8-61', 5797, 5804);
+insert into clasps (nt_quotation_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (261, 'Psalms', 'LXX Psalms 68:18 68:18-53', 'Ephesians', 'SBLGNT Ephesians 4:8+8 4:8-3', 73397, 56, 5805, 58);
 
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (271, 27, 1, 'Psalms', 69, 'Psalms 69:9a', 'John', 'John 2:17', null, null, null, null, 'traditional');
