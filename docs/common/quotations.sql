f8f936c87001048bb1c583a529ff8e425f89489f
Zoltán Kovács <zoltan@geogebra.org>
2021-04-30 07:06:30 +0200

    Add i/c data for Matthew 4:6-7

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -805,6 +805,12 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (34, 1, null, 'γεγραπται γαρ'); -- γεγραπται γαρ = it has been written for
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (34, 1, 'quotation', 'literal, superset', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (341, 'Matthew', 'SBLGNT Matthew 4:6+42 4:6-92', 6554, 6565);
+insert into clasps (nt_quotation_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (341, 'Psalms', 'LXX Psalms 91:11 91:11-35', 'Matthew', 'SBLGNT Matthew 4:6+54 4:6-55', 107356, 37, 6566, 37);
+insert into clasps (nt_quotation_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (341, 'Psalms', 'LXX Psalms 91:12 91:12', 'Matthew', 'SBLGNT Matthew 4:6+94 4:6', 107428, 52, 6606, 52);
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (342, 34, 2, 'Psalms', 91, 'Psalms 91:11-12', 'Luke', 'Luke 4:10-11', null, null, null, null, 'traditional');
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
