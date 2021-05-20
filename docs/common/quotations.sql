67ab61e002d2355518ef94143fb074728aef1281
Zoltán Kovács <zoltan@geogebra.org>
2021-05-20 07:01:11 +0200

    Add i/c data for Romans 15:11

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -1165,6 +1165,10 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (44, 1, null, 'και παλιν'); -- και παλιν = and again
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (44, 1, 'quotation', 'literal, fuzzy', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (441, 'Romans', 'SBLGNT Romans 15:11 15:11-60', 30257, 30264);
+insert into clasps (nt_quotation_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (441, 'Psalms', 'LXX Psalms 117:1+9 117:1', 'Romans', 'SBLGNT Romans 15:11+8 15:11', 137217, 54, 30265, 60);
 
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (451, 45, 1, 'Psalms', 118, 'Psalms 118:6', 'Hebrews', 'Hebrews 13:6', null, null, null, null, 'traditional');
