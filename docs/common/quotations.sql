86ba42950af4cb08549fb65e7a6daf41fb9beb9b
Zoltán Kovács <zoltan@geogebra.org>
2021-06-08 08:51:29 +0200

    Add i/c data for Romans 10:16

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -1485,6 +1485,10 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (109, 1, 'Ησαιας', 'γαρ λεγει'); -- γαρ λεγει = for says
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (109, 1, 'quotation', 'literal, exact', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (1091, 'Romans', 'SBLGNT Romans 10:16+31 10:16-28', 21702, 21715);
+insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (1091, 109, 1, 'Isaiah', 'LXX Isaiah 53:1 53:1-31', 'Romans', 'SBLGNT Romans 10:16+45 10:16', 107401, 28, 21716, 28);
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (1092, 109, 2, 'Isaiah', null, 'LXX Isaiah 52:15+123 53:1', 'John', 'SBLGNT John 12:38+39 12:38', 107400, 60, 44175, 60, 'getrefs');
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
