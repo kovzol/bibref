f847a501d19787f52db2bffcd008c3a27d600a94
Zoltán Kovács <zoltan@geogebra.org>
2021-07-03 12:10:51 +0200

    Add i/c data for Hebrews 2:13

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -1805,6 +1805,14 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (128, 1, null, 'και παλιν ... και παλιν'); -- και παλιν = and again
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (128, 1, 'quotation', 'literal, fuzzy', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (1281, 'Hebrews', 'SBLGNT Hebrews 2:13 2:13-64', 2401, 2408);
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (1281, 'Hebrews', 'SBLGNT Hebrews 2:13+31 2:13-33', 2432, 2439);
+insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (1281, 128, 1, 'Isaiah', 'LXX Isaiah 8:17+67 8:17', 'Hebrews', 'SBLGNT Hebrews 2:13+11 2:13-41', 17111, 20, 2412, 20);
+insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (1281, 128, 1, 'Isaiah', 'LXX Isaiah 8:18 8:18-78', 'Hebrews', 'SBLGNT Hebrews 2:13+39 2:13', 17131, 33, 2440, 33);
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (1291, 129, 1, 'Isaiah', null, 'LXX Isaiah 26:20+66 26:20-23', 'Hebrews', 'SBLGNT Hebrews 10:37+6 10:37-27', 49503, 14, 18052, 14, 'getrefs');
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
