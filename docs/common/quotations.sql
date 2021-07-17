44e31a91a7906a2be0cf6d65af6233923cc8e386
Zoltán Kovács <zoltan@geogebra.org>
2021-07-17 09:21:59 +0200

    Add i/c data for Acts 7:49-50

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -2126,6 +2126,10 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (142, 1, 'ο προφητης', 'καθως ... λεγει'); -- καθως ... λεγει = as ... says
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (142, 1, 'quotation', 'literal, fuzzy', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (1421, 'Acts', 'SBLGNT Acts 7:48+37 7:48', 22652, 22670);
+insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (1421, 142, 1, 'Isaiah', 'LXX Isaiah 66:1+5 66:2-105', 'Acts', 'SBLGNT Acts 7:49 7:50', 130323, 132, 22671, 134);
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (1431, 143, 1, 'Isaiah', null, 'LXX Isaiah 53:7+40 53:7-43', 'Acts', 'SBLGNT Acts 8:32+38 8:32-43', 108067, 48, 26456, 48, 'getrefs');
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
