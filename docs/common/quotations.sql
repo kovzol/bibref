4e3cfd2beed9dc609a54d171bc1cf2952fe30fc4
Zoltán Kovács <zoltan@geogebra.org>
2021-07-28 12:53:07 +0200

    Add i/c data for Galatians 3:11

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -2284,6 +2284,10 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (1001, 2, null, 'οτι'); -- οτι = because
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (1001, 2, 'quotation', 'literal, superset', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (10012, 'Galatians', 'SBLGNT Galatians 3:11 3:11-24', 4470, 4513);
+insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (10012, 1001, 2, 'Habakkuk', 'LXX Habakkuk 2:4+39 2:4', 'Galatians', 'SBLGNT Galatians 3:11+44 3:11', 2127, 29, 4514, 24);
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (1291, 1001, 3, 'Habakkuk', null, 'LXX Habakkuk 2:3+84 2:3-9', 'Hebrews', 'SBLGNT Hebrews 10:37+21 10:37-8', 2061, 18, 18067, 18, 'getrefs');
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
