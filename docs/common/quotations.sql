917e9c83405227827652939073f45385990b1880
Zoltán Kovács <zoltan@geogebra.org>
2021-08-01 21:24:56 +0200

    Add i/c data for Acts 13:41

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -2320,6 +2320,12 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (1002, 1, 'εν τοις προφηταις', 'ειρημενον'); -- ειρημενον εν τοις προφηταις = having been said
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (1002, 1, 'quotation', 'literal, fuzzy, superset', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (10021, 'Acts', 'SBLGNT Acts 13:40 13:40', 44333, 44376);
+insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (10021, 1002, 1, 'Habakkuk', 'LXX Habakkuk 1:5', 'Acts', 'SBLGNT Acts 13:41 13:41-4', 343, 135, 44377, 117);
+-- The clasp has difference 0.179, however, by adding the last four letters from Acts, 0.171 could be reached.
+-- Also, the clasp could be split into two shorter ones, because the word 'και επιβλεψατε' could be omitted.
 
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (11011, 1101, 1, 'Malachi', null, 'LXX Malachi 1:3 1:3-68', 'Romans', 'SBLGNT Romans 9:13+29 9:13', 174, 16, 19062, 16, 'getrefs');
