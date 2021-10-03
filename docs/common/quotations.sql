2db9308970b8606f7943298d6fd936db80fecf5d
Zoltán Kovács <zoltan@geogebra.org>
2021-10-03 13:01:36 +0200

    Add i/c data for Acts 3:25

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -3011,6 +3011,10 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (2016, 1, 'προς Αβρααμ', 'θεος ... λεγων'); -- θεος ... λεγων = God ... saying
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (2016, 1, 'quotation', 'literal, fuzzy', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (20161, 'Acts', 'SBLGNT Acts 3:25 3:25-54', 9079, 9166);
+insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (20161, 2016, 1, 'Genesis', 'LXX Genesis 28:14+98 28:14', 'Acts', 'SBLGNT Acts 3:25+88 3:25', 76468, 57, 9167, 54);
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (20171, 2017, 1, 'Genesis', null, 'LXX Genesis 12:1+21 12:1-43', 'Acts', 'SBLGNT Acts 7:3+17 7:3-26', 27636, 41, 18379, 41, 'getrefs');
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
