459d3c75155d7a2c1c5563157deff953511bd02d
Zoltán Kovács <zoltan@geogebra.org>
2021-11-03 08:41:52 +0100

    Fix previous commit

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -3395,7 +3395,7 @@ insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book,
 insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
  (21132, 2113, 2, 'Exodus', 'LXX Exodus 3:6+19 3:6-116', 'Mark', 'SBLGNT Mark 12:26+91 12:26-32', 4997, 5, 40551, 5); -- ο θεος
 insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
- (21132, 2113, 2, 'Exodus', 'LXX Exodus 3:6+40 3:6-70', 'Mark', 'SBLGNT Mark 12:26+96 12:26', 5018, 32, 40556, 30); -- αβρααμ και θεος ισαακ και θεος ιακωβ -> Αβρααμ και ο θεος Ισαακ και ο θεος Ιακωβ
+ (21132, 2113, 2, 'Exodus', 'LXX Exodus 3:6+40 3:6-70', 'Mark', 'SBLGNT Mark 12:26+96 12:26', 5018, 30, 40556, 32); -- αβρααμ και θεος ισαακ και θεος ιακωβ -> Αβρααμ και ο θεος Ισαακ και ο θεος Ιακωβ
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (21133, 2113, 3, 'Exodus', null, 'LXX Exodus 3:6+33 3:6-70', 'Luke', 'SBLGNT Luke 20:37+64 20:37', 5011, 37, 78894, 37, 'manual');
 insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_given, as_it_is_written) values
