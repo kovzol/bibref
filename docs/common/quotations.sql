4798e8582dce649bb5f14e7a25ab07dc728aa1fb
Zoltán Kovács <zoltan@geogebra.org>
2021-11-13 09:44:39 +0100

    Add i/c data for Mark 12:31

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -3545,6 +3545,10 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (2202, 4, 'εντολη', 'αυτη'); -- εντολη = commandment, αυτη = this
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (2202, 4, 'quotation', 'literal, exact', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (22024, 'Mark', 'SBLGNT Mark 12:31 12:31-61', 40925, 40935);
+insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (22024, 2202, 4, 'Leviticus', 'LXX Leviticus 19:18+57 19:18-13', 'Mark', 'SBLGNT Mark 12:31+11 12:31-30', 60912, 31, 40936, 31);
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (22025, 2202, 5, 'Leviticus', null, 'LXX Leviticus 19:18+57 19:18-13', 'Romans', 'SBLGNT Romans 13:9+102 13:9', 60912, 31, 27293, 31, 'getrefs');
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
