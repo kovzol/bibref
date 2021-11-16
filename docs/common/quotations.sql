4b633c120232108dd1d50b40dbe7c624c1de6b90
Zoltán Kovács <zoltan@geogebra.org>
2021-11-16 09:58:50 +0100

    Fix previous entry

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -3582,7 +3582,7 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (2202, 7, 'quotation', 'literal, exact', 'Z.K.');
 insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
- (22027, 'Matthew', 'Matthew 22:39 22:39-31', 65838, 65855);
+ (22027, 'Matthew', 'SBLGNT Matthew 22:39 22:39-31', 65838, 65855);
 insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
  (22027, 2202, 7, 'Leviticus', 'LXX Leviticus 19:18+57 19:18-13', 'Matthew', 'SBLGNT Matthew 22:39+18 22:39', 60912, 31, 65856, 31);
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
