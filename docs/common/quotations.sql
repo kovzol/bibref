1a72f5d7e0cabfb1440cd225d40e31445a346bde
Zoltán Kovács <zoltan@geogebra.org>
2021-12-24 16:56:46 +0100

    Add i/c data for Hebrews 12:5-6

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -4081,6 +4081,10 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (3102, 1, null, 'παρακλησεως'); -- παρακλησεως = exhortation
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (3102, 1, 'quotation', 'literal, exact', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (31021, 'Hebrews', 'SBLGNT Hebrews 12:5 12:5-58', 22284, 22334);
+insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (31021, 3102, 1, 'Proverbs', 'LXX Proverbs 3:11 3:12', 'Hebrews', 'SBLGNT Hebrews 12:5+51 12:6', 4273, 111, 22335, 114);
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (31031, 3103, 1, 'Proverbs', null, 'LXX Proverbs 3:34+4 3:34', 'James', 'SBLGNT James 4:6+32 4:6', 5754, 48, 6153, 48, 'getrefs');
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
