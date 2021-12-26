9c9a7b2243de5c5a3c4265bf0fb04ca5ba8d8527
Zoltán Kovács <zoltan@geogebra.org>
2021-12-26 08:21:01 +0100

    Add i/c data for Hebrews 1:5b

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -4126,6 +4126,10 @@ insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_give
  (4101, 1, null, 'και παλιν'); -- και παλιν = and again
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (4101, 1, 'quotation', 'literal, exact', 'Z.K.');
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (41011, 'Hebrews', 'SBLGNT Hebrews 1:5+58 1:5-45', 464, 471);
+insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
+ (41011, 4101, 1, 'II_Samuel', 'LXX II_Samuel 7:14 7:14-71', 'Hebrews', 'SBLGNT Hebrews 1:5+66 1:5', 20344, 45, 472, 45);
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (42011, 4201, 1, 'I_Kings', null, 'LXX I_Kings 19:10+72 19:10-94', 'Romans', 'SBLGNT Romans 11:3+30 11:3-39', 75874, 28, 22370, 28, 'getrefs');
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
