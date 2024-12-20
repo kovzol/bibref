80ee5f2aa60187843f78ed2dc4ac7a0b22575594
kovzol <zoltan@geogebra.org>
2024-12-20 21:15:43 +0100

    Fix q206

--- docs/common/quotations.sql
+++ docs/common/quotations.sql
@@ -670,11 +670,13 @@ insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passag
 insert into quotations (nt_quotation_id, ot_id, nt_id, ot_book, psalm, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length, found_method) values
  (241, 24, 1, 'Psalms', 45, 'LXX Psalms 45:6 45:7', 'Hebrews', 'SBLGNT Hebrews 1:8+13 1:9', 50077, 175, 713, 181, 'manual');
 insert into quotations_properties (quotation_ot_id, quotation_nt_id, source_given, as_it_is_written) values
- (24, 1, null, 'λεγει ... προς δε τον υιον'); -- λεγει ... προς δε τον υιον =  He says ... unto however the Son
+ (24, 1, 'θεος', 'προς δε τον υιον'); -- λεγει ... προς δε τον υιον =  He says ... unto however the Son
 insert into quotations_classifications (quotation_ot_id, quotation_nt_id, classification, classification_subtype, classified_by) values
  (24, 1, 'quotation', 'literal', 'Z.K.');
 insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
  (241, 'Hebrews', 'SBLGNT Hebrews 1:8 1:8-78', 700, 712);
+insert into nt_quotation_introductions (nt_quotation_id, nt_book, nt_passage, nt_startpos, nt_endpos) values
+ (241, 'Hebrews', 'SBLGNT Hebrews 1:1+28 1:1-27', 41, 51); -- Theos
 insert into clasps (nt_quotation_id, ot_id, nt_id, ot_book, ot_passage, nt_book, nt_passage, ot_startpos, ot_length, nt_startpos, nt_length) values
  (241, 24, 1, 'Psalms', 'LXX Psalms 45:6 45:7', 'Hebrews', 'SBLGNT Hebrews 1:8+13 1:9', 50077, 175, 713, 181);
 
