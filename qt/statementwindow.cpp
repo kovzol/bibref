9af4ff63953ac07dc3ab0e1875863848d4f285c7
Zoltán Kovács <zoltan@geogebra.org>
2025-04-18 18:18:31 +0200

    Fix a-y to a-z

--- qt/statementwindow.cpp
+++ qt/statementwindow.cpp
@@ -184,11 +184,11 @@ void StatementWindow::setupEditor()
         settings.setValue("Statements/text", QString("") + "Statement Matthew-1,23 connects\n" +
                                                  " SBLGNT Matthew 1:23 1:23-34 (1922-1994) with\n" +
                                                  " LXX Isaiah 7:14+35 7:14 (14234-14304) based on\n" +
-                                                 "  introduction 1:22 1:22 (1856-1921) a-y form toytodeolongegoneninaplhrvuhtorhuenypokyrioydiatoyprofhtoylegontos that\n" +
+                                                 "  introduction 1:22 1:22 (1856-1921) a-z form toytodeolongegoneninaplhrvuhtorhuenypokyrioydiatoyprofhtoylegontos that\n" +
                                                  "   declares a quotation with 'το ρηθεν υπο κυριου ... λεγοντος' also\n" +
                                                  "   identifies the source with 'δια του προφητου' moreover\n" +
-                                                 "  fragment 1:23 1:23-34 (1922-1994, length 73) a-y form idoyhparuenosengastriejeikaitejetaiyionkaikalesoysintoonomaaytoyemmanoyhl\n" +
-                                                 "   matches LXX Isaiah 7:14+35 7:14 (14234-14304, length 71) a-y form idoyhparuenosengastriejeikaitejetaiyionkaikaleseistoonomaaytoyemmanoyhl\n" +
+                                                 "  fragment 1:23 1:23-34 (1922-1994, length 73) a-z form idoyhparuenosengastriejeikaitejetaiyionkaikalesoysintoonomaaytoyemmanoyhl\n" +
+                                                 "   matches LXX Isaiah 7:14+35 7:14 (14234-14304, length 71) a-z form idoyhparuenosengastriejeikaitejetaiyionkaikaleseistoonomaaytoyemmanoyhl\n" +
                                                  "    unique in Old Testament\n" +
                                                  "   differing by 8.33%\n" +
                                                  "  providing an overall cover of 100.00%.");
