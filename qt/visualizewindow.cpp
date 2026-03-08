5ef034cee3d902f23d1616a00f22c19773606fc0
Zoltán Kovács <zoltan@geogebra.org>
2026-03-08 07:23:33 +0100

    Separate GraphViz related code

--- qt/visualizewindow.cpp
+++ qt/visualizewindow.cpp
@@ -1,5 +1,6 @@
 #include "visualizewindow.h"
 #include "settings.h"
+#include "gv-export.h"
 
 #include <QtSvg>
 #include <QSvgWidget>
@@ -39,38 +40,7 @@ VisualizeWindow::VisualizeWindow(QWidget *parent, string input)
         setCentralWidget(tile);
     }
 
-    GVC_t *gvc= gvContext();
-#if defined(__MINGW32__) || defined(__APPLE__) || defined(__EMSCRIPTEN__)
-    /* This seems to be required on Windows, MacOS and the WebAssembly platform, otherwise the
-     * executable will not know anything about the dot format,
-     * the svg export, and so on.
-     * Also, in CMake's library settings these 4 dynamic libraries
-     * must be included. There seems to be no official way to
-     * do this elegantly.
-     */
-    extern gvplugin_library_t gvplugin_dot_layout_LTX_library;
-    extern gvplugin_library_t gvplugin_core_LTX_library;
-    gvAddLibrary(gvc, &gvplugin_dot_layout_LTX_library);
-    gvAddLibrary(gvc, &gvplugin_core_LTX_library);
-#ifndef __EMSCRIPTEN__ // Interestingly, Emscripten does not compile these and they are not required, either:
-    extern gvplugin_library_t gvplugin_rsvg_LTX_library;
-    extern gvplugin_library_t gvplugin_pango_LTX_library;
-    gvAddLibrary(gvc, &gvplugin_rsvg_LTX_library);
-    gvAddLibrary(gvc, &gvplugin_pango_LTX_library);
-#endif // __EMSCRIPTEN__
-#endif // all platforms that require such definitions
-
-    Agraph_t *g = agmemread((char*)input.c_str());
-    gvLayout(gvc, g, "dot");
-
-#define MAX_SVG_LENGTH ((GVRENDERDATA_SIZE) 200000)
-    char *svg = (char*) malloc(MAX_SVG_LENGTH);
-    GVRENDERDATA_SIZE length;
-    gvRenderData(gvc, g, "svg", &svg, &length);
-    gvFreeLayout(gvc, g);
-    agclose(g);
-    gvFreeContext(gvc);
-    string svg_s = string(svg);
+    string svg_s = string(gv_export(input, "svg"));
 
     // This is used only for diagramHtml (at the moment):
     int widthPosS = svg_s.find("<svg width=\"") + 12;
